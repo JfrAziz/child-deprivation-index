bd2d4ec1b9af6ad4caceb30845aec4ba34d3d28a
Ignatius Aditya Setyadi <ignatius.aditya.setyadi@gmail.com>
2024-01-26 12:51:06 +0700

    feat: add bps X unicef collaboration

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -12,34 +12,36 @@ export const Hero = () => {
         }}
       >
         <div className="hero min-h-screen">
-          <div className="hero-content flex flex-col gap-y-8">
-            {/* <div className="-mt-16 flex flex-auto gap-2 text-center">
-              <div className="flex w-72 flex-auto rounded-full bg-background px-6 py-3 drop-shadow-[0_7px_80px_theme(colors.secondary)]">
+          <div className="hero-content flex flex-col gap-y-[clamp(1.4rem,3vw,3rem)]">
+            <div className="flex justify-evenly gap-[clamp(0.2rem,2vw,1rem)]">
+              <div className="px-[clamp(0.2rem,6vw,2rem)] py-[clamp(0.5rem,3vw,1rem)] bg-background flex rounded-full">
                 <span>
                   <img
-                    className="h-8"
+                    className="h-[clamp(0.8rem,1.6vw,2rem)]"
                     alt="bps"
                     src="https://www.bps.go.id/_next/image?url=%2Fassets%2Flogo-bps.png&w=1080&q=75"
                   />
                 </span>
-                <p className="font-arial ml-2 mt-[0.25em] text-[1em] font-bold uppercase italic text-blue-500">
+                <p className="font-arial ml-[clamp(0.4rem,0.6vw,1rem)] text-[clamp(0.5rem,1vw,1rem)] font-bold uppercase italic text-blue-500">
                   Statistics Indonesia
                 </p>
+                
               </div>
-              <div className="w-72 rounded-full bg-[#00AEEC] px-8 py-3 drop-shadow-[0_7px_80px_theme(colors.secondary)]">
+              
+              <div className="px-[clamp(0.2rem,6vw,2rem)] py-[clamp(0.5rem,3vw,1rem)] bg-[#00AEEC] flex rounded-full">
                 <span>
                   <img
-                    className=""
+                    className="h-[clamp(0.8rem,1.6vw,2rem)]"
                     alt="unicef"
                     src="https://www.unicef.org/sites/default/files/styles/logo/public/English_9.png.webp"
                   />
-                </span>
+                </span>                
               </div>
-            </div> */}
+            </div>
 
             <div className="flex flex-col justify-center gap-y-[clamp(0.2rem,1vw,1rem)] text-center">
               <div className=" drop-shadow-[0_7px_130px_theme(colors.secondary)]">
-                <h1 className="font-title !text-center text-[clamp(1rem,4vw,3rem)] font-bold leading-[1.1] mb-[2em">
+                <h1 className="font-title !text-center text-[clamp(1.2rem,4vw,3rem)] font-bold leading-[1.1] mb-[2em">
                   <span className="inline-grid text-center">
                     <span className="[&::selection]:text-neutral-content relative col-start-1 row-start-1 text-base-content drop-shadow-[0_3px_4px_#1f2937]">
                     From the Sky to the Ground:
